1c3b0932843377f7d7d4a6d2446ccc3d4ee9d575
Liliia Galkina <vvlili@yandex.ru>
2025-05-19 20:12:13 +0300

    стили для pricing с адаптивом

--- js/script.js
+++ js/script.js
@@ -1,61 +1,79 @@
-"use strict"
+"use strict";
 
 //меню-бургер
 
-let burger = document.querySelector('#burger');
-let menu = document.querySelector('#menu');
+let burger = document.querySelector("#burger");
+let menu = document.querySelector("#menu");
 
-
-burger.addEventListener('click', func);
+burger.addEventListener("click", func);
 
 function func() {
-   burger.classList.toggle('menu__burger_active');
-	menu.classList.toggle('menu__list_active');
+  burger.classList.toggle("menu__burger_active");
+  menu.classList.toggle("menu__list_active");
 }
 
 // слайдер
 
 $(document).ready(function () {
-	$('.slider').slick({
-		infinite: false,
-		slidesToShow: 2,
-		responsive: [
-			{
-				breakpoint: 769,
-				settings: {
-					slidesToShow: 1
-				}
-			}
-		]
-	})
+  $(".slider").slick({
+    infinite: false,
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
 
 //изменение стрелок при прокрутке слайдов
 
 document.addEventListener("DOMContentLoaded", () => {
-	const prevArrow = document.querySelector(".slick-prev");
-	const nextArrow = document.querySelector(".slick-next");
-	const slides = document.querySelectorAll(".slider__item");
-	
-	const arrowCheck = (arrow) => {
-		if (arrow.classList.contains("slick-disabled")) {
-			arrow.style.opacity = 0.2;
-		} else {
-			arrow.style.opacity = 1;
-		}
-	}
+  const prevArrow = document.querySelector(".slick-prev");
+  const nextArrow = document.querySelector(".slick-next");
+  const slides = document.querySelectorAll(".slider__item");
 
-	arrowCheck(prevArrow);
-	arrowCheck(nextArrow);
+  const arrowCheck = (arrow) => {
+    if (arrow.classList.contains("slick-disabled")) {
+      arrow.style.opacity = 0.2;
+    } else {
+      arrow.style.opacity = 1;
+    }
+  };
 
-	nextArrow.addEventListener("click", () => {
-		arrowCheck(prevArrow);
-   	arrowCheck(nextArrow);
-	})
+  arrowCheck(prevArrow);
+  arrowCheck(nextArrow);
 
-		prevArrow.addEventListener("click", () => {
-      arrowCheck(prevArrow);
-      arrowCheck(nextArrow);
-    });
+  nextArrow.addEventListener("click", () => {
+    arrowCheck(prevArrow);
+    arrowCheck(nextArrow);
+  });
 
-})
+  prevArrow.addEventListener("click", () => {
+    arrowCheck(prevArrow);
+    arrowCheck(nextArrow);
+  });
+});
+
+//prising-mobile
+
+const features1 = document.querySelector("#features1");
+const featuresItems1 = document.querySelector("#features-items1");
+const featuresArrow1 = document.querySelector("#features-arrow1");
+
+const features2 = document.querySelector("#features2");
+const featuresItems2 = document.querySelector("#features-items2");
+const featuresArrow2 = document.querySelector("#features-arrow2");
+
+features1.addEventListener("click", function() {
+  featuresArrow1.classList.toggle("features-arrow-rotate");
+  featuresItems1.classList.toggle("features-items-show");
+});
+
+features2.addEventListener("click", function() {
+  featuresArrow2.classList.toggle("features-arrow-rotate");
+  featuresItems2.classList.toggle("features-items-show");
+});
